8b82fb17346398a7f2aae58822eaffa02addbceb
anonyaro <anonyaro@users.noreply.github.com>
2026-01-18 17:39:25 +0500

    XFIChecker main entrypoint source

---
+++ xfichecker.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 anonyaro (github.com/anonyaro)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * --- MANDATORY ATTRIBUTION NOTICE ---
+ * In accordance with Section 7(b) of the GNU AGPLv3, the following
+ * appropriate legal notices must be preserved and displayed in the
+ * user interface of any work incorporating this software:
+ *
+ * Author: anonyaro (github.com/anonyaro)
+ * Project: XFIChecker (https://github.com/anonyaro/XFIChecker)
+ * Support: https://dalink.to/xenyaro
+ */
+
+#include "cli.h"
+#include <exception>
+#include <iostream>
+#include "cli_msgs.h"
+
+int main(int argc, char* argv[]) {
+    try {
+        CLI xfic(argc, argv); 
+        xfic.run();          
+    }
+    catch (const std::exception& ex) {
+        std::cerr << xfic::errorMsg::X_ERROR << ex.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
